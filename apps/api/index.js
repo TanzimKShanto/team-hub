4e473d6f381804195c381ad236b386a4917da55b
Tanzim Khan Shanto <tanzimk.shanto@gmail.com>
2026-05-03 23:35:56 +0600

    debugging

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+console.log('CLIENT_URL:', process.env.CLIENT_URL)
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
